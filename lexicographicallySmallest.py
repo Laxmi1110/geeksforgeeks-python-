3d01d2f6bb151c8dd90de6af40c5507d83d8d618
Laxmi Gupta <laxmigupta188777@gmail.com>
2026-06-05 20:03:12 +0530

    Implement lexicographically smallest string function

---
+++ lexicographicallySmallest.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lexicographicallySmallest(self, s, k):
+        n = len(s)
+
+        # Check if n is a power of 2
+        if n & (n - 1) == 0:
+            k //= 2
+        else:
+            k *= 2
+
+        if k >= n:
+            return "-1"
+
+        stack = []
+
+        for ch in s:
+            while stack and k > 0 and stack[-1] > ch:
+                stack.pop()
+                k -= 1
+            stack.append(ch)
+
+        while k > 0:
+            stack.pop()
+            k -= 1
+
+        ans = ''.join(stack)
+
+        return ans if ans else "-1"
